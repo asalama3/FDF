9f17d95730b67643ebb53301e2046716d5b733ba
Andrea SALAMA <asalama@e3r6p17.42.fr>
2016-01-27 17:10:04 +0100

    fdf

---
+++ main.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asalama <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/01/27 13:39:43 by asalama           #+#    #+#             */
+/*   Updated: 2016/01/27 17:07:11 by asalama          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <mlx.h>
+
+int		main()
+{
+	void	*mlx;
+	void	*win;
+	int		x;
+	int		y;
+
+	mlx = mlx_init();
+	win = mlx_new_window(mlx, 400, 400, "mlx 42");
+	y = 50;
+	while (y < 150)
+	{
+		x = 50;
+		while (x < 150)
+		{
+			mlx_pixel_put(mlx, win, x, y, 0x00FFFFFF);
+			x++;
+		}
+		y++;
+	}
+	mlx_loop(mlx); // donner la main a lensemble du system graphique mac qui se charge d'affichier la fenetre.
+}
+
+// 
