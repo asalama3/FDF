7cd4b21f2cfd0a8e04de360fdce6672d9720efa0
JEREMY MARUANI <jeremymaruani@iMac-de-JEREMY.local>
2016-02-18 19:33:50 +0100

    fonctions relief et rotation

---
+++ rotate.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   move.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asalama <asalama@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/02/10 14:56:17 by asalama           #+#    #+#             */
+/*   Updated: 2016/02/16 20:36:18 by asalama          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+
+void		rotate_1(t_env *env, t_tab *tab, t_angle *angle)	
+{
+	mlx_clear_window(env->mlx, env->win);
+	mlx_destroy_image(env->mlx, env->img);
+    angle->a += ROTATE_1;
+    image(env, tab, angle);
+}
+
+void		rotate_2(t_env *env, t_tab *tab, t_angle *angle)
+{
+	mlx_clear_window(env->mlx, env->win);
+	mlx_destroy_image(env->mlx, env->img);
+    angle->a -= ROTATE_2;
+    image(env, tab, angle);
+}
---
+++ zoom_relief.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   move.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asalama <asalama@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/02/10 14:56:17 by asalama           #+#    #+#             */
+/*   Updated: 2016/02/16 20:36:18 by asalama          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+
+void		zoom_in(t_env *env, t_tab *tab, t_angle *angle)	
+{
+	mlx_clear_window(env->mlx, env->win);
+	mlx_destroy_image(env->mlx, env->img);
+	env->space += ZOOM;
+	image(env, tab, angle);
+//    printf("env space:%d\n", env->space);
+}
+
+void		zoom_out(t_env *env, t_tab *tab, t_angle *angle)	
+{
+	mlx_clear_window(env->mlx, env->win);
+	mlx_destroy_image(env->mlx, env->img);
+	env->space -= ZOOM;
+	image(env, tab, angle);
+  //  printf("env->space:%d\n", env->space);
+}
+
+void        relief_z_up(t_env *env, t_tab *tab, t_angle *angle)
+{
+    int     i;
+    int     j;
+
+    ft_putstr("OK\n");
+	mlx_clear_window(env->mlx, env->win);
+	mlx_destroy_image(env->mlx, env->img);
+	j = 0;
+    while (j < tab->size_ver)
+    {
+        i = 0;
+        while (i < tab->size_hor)
+        {
+            if (tab->tab_int[j][i] != '\0')
+                tab->tab_int[j][i] += RELIEF;
+                printf("%i\n", tab->tab_int[j][i]);
+            i++;
+        }
+        j++;
+    }
+//    printf("tab:%i\n", tab->tab_int[j][i]);
+    image(env, tab, angle);
+}
+
+void        relief_z_down(t_env *env, t_tab *tab, t_angle *angle)
+{
+    int     i;
+    int     j;
+
+    ft_putstr("OK\n");
+	mlx_clear_window(env->mlx, env->win);
+	mlx_destroy_image(env->mlx, env->img);
+	j = 0;
+    while (j < tab->size_ver)
+    {
+        i = 0;
+        while (i < tab->size_hor)
+        {
+            if (tab->tab_int[j][i] != '\0')
+                tab->tab_int[j][i] -= RELIEF;
+                printf("%i\n", tab->tab_int[j][i]);
+            i++;
+        }
+        j++;
+    }
+//    printf("tab:%i\n", tab->tab_int[j][i]);
+    image(env, tab, angle);
+}
